07ad0a3cd8de1e1973ca1dbb9f8052a59b57a89c
Leticia Cardoso <159138722+crdsleticia@users.noreply.github.com>
2024-05-07 09:17:21 -0300

    Add files via upload

---
+++ spyder - trabalho 1.py
@@ -0,0 +1,6 @@
+def imprimir_informacoes(nome, idade, cidade):
+    print("Nome:", nome, "- Idade:", idade, "- Cidade:", cidade, sep=" ", end="!\n")
+
+imprimir_informacoes("Lele", 25, "Minas Gerais")
+
+
